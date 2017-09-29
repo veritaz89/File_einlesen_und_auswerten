55a3b58741582976dfaa9bc293f15b2fe5fb9fe1
veritaz89 <alex.briese@googlemail.com>
2017-09-29 08:45:09 +0200

    start
    start

---
+++ File_einlesen_und_auswerten/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_einlesen_und_auswerten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
